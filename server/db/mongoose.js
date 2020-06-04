96463639d036cb8e15d6eee27dd6236a3b1794f8
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-06-04 15:54:51 +0530

    first deploy

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true ,useCreateIndex: true,useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://todoNodeApi:Z319Xe4qnWhDs6uB@cluster0-xw78b.mongodb.net/todNodeApi?retryWrites=true&w=majority',
+      { useNewUrlParser: true ,useCreateIndex: true,useUnifiedTopology: true },()=>{
+          console.log("Connected to Database")
+      });
 // mongodb+srv://debarghyaapi:UbI2S5gSyxpkXYiI@cluster0-eefvp.mongodb.net/test?retryWrites=true&w=majority
 module.exports={mongoose};
